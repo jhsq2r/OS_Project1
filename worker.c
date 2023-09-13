e5e5183cb237edc892afb14af0e13b8d0bc0d069
jhsq2r <126211836+jhsq2r@users.noreply.github.com>
2023-09-13 07:30:24 -0500

    Update worker.c

--- worker.c
+++ worker.c
@@ -4,7 +4,12 @@
 
 int main(int argc, char** argv){
 
-        printf("Takeover works");
+        for(int x = 0; x < atoi(argv[1]); x++){
+                printf("WORKER PID:%d PPID:%d Iteration:%d before sleeping\n", getpid(),getppid(),x+1);
+                sleep(1);
+                printf("WORKER PID:%d PPID:%d Iteration:%d after sleeping\n", getpid(),getppid(),x+1);
+        }
+
         return 0;
 }
 
